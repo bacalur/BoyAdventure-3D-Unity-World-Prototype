deac57d853cb92f233d63e279f9e2ccb0fcba3d7
bacalur <128888984+bacalur@users.noreply.github.com>
2024-05-16 20:25:39 +0300

    Update FootstepSound.cs

--- Assets/Scripts/Audio/FootstepSound.cs
+++ Assets/Scripts/Audio/FootstepSound.cs
@@ -2,19 +2,17 @@ using UnityEngine;
 
 public class FootstepSound : MonoBehaviour
 {
-    public AudioClip footstepSound; // Звуковой файл для шагов
-    public AudioClip jumpSound; // Звуковой файл для прыжка
-    public float stepInterval = 0.5f; // Интервал между шагами
+    public AudioClip footstepSound;
+    public AudioClip jumpSound;
+    public float stepInterval = 0.5f;
     private float nextStepTime = 0f;
 
-    private AudioSource audioSource; // Ссылка на компонент AudioSource
+    private AudioSource audioSource;
 
     void Start()
     {
-        // Получаем компонент AudioSource
         audioSource = GetComponent<AudioSource>();
 
-        // Проверяем, есть ли компонент AudioSource. Если нет, добавляем его.
         if (audioSource == null)
         {
             audioSource = gameObject.AddComponent<AudioSource>();
@@ -23,20 +21,15 @@ public class FootstepSound : MonoBehaviour
 
     void Update()
     {
-        // Проверяем, нажата ли клавиша движения и прошло ли достаточно времени для следующего шага
         if ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D)) && Time.time > nextStepTime)
         {
-            // Проигрываем звук шага
             audioSource.PlayOneShot(footstepSound);
 
-            // Обновляем время следующего шага
             nextStepTime = Time.time + stepInterval;
         }
 
-        // Проверяем, была ли нажата клавиша пробела для прыжка
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            // Проигрываем звук прыжка
             audioSource.PlayOneShot(jumpSound);
         }
     }
