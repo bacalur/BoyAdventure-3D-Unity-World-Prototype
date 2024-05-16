9c3659e35c7fb10f398e3646b55c773caf9e744f
bacalur <128888984+bacalur@users.noreply.github.com>
2024-05-16 20:22:40 +0300

    Update SceneSwitcher.cs

--- Assets/Scripts/SceneSwitcher.cs
+++ Assets/Scripts/SceneSwitcher.cs
@@ -5,10 +5,8 @@ public class SceneSwitcher : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        // Проверяем, с чем столкнулся персонаж
         if (other.CompareTag("Skull"))
         {
-            // Переходим в сцену "project_01"
             SceneManager.LoadScene("project_01");
         }
     }
