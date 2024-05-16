d773522d17ebbc5024e811ed603738ba22f443ba
bacalur <128888984+bacalur@users.noreply.github.com>
2024-05-16 20:26:24 +0300

    Update FollowPlayer.cs

--- Assets/Scripts/Camera/FollowPlayer.cs
+++ Assets/Scripts/Camera/FollowPlayer.cs
@@ -2,14 +2,13 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-    public Transform player; // Ссылка на трансформ персонажа
-    public Vector3 offset = new Vector3(0f, 2f, -5f); // Расстояние и высота от персонажа
+    public Transform player;
+    public Vector3 offset = new Vector3(0f, 2f, -5f);
 
     void Update()
     {
         if (player != null)
         {
-            // Устанавливаем позицию камеры на позицию персонажа с учетом смещения
             transform.position = player.position + offset;
         }
     }
